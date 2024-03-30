b5d7f4b181f9fcb24c00ac6a5d9c5cff15895f7d
Mike Kostis <71893248+Mikostis@users.noreply.github.com>
2024-03-30 20:33:31 +0200

    Update air-quality.js

--- api/air-quality/controllers/air-quality.js
+++ api/air-quality/controllers/air-quality.js
@@ -34,7 +34,7 @@ module.exports = {
   },
   async fetchParisAirPollution(ctx) {
     try {
-      const apiKey = "0d132a15-ab92-4b8b-8ff6-45d883a8b1f5";
+      const api_key="yours api key";
       const apiUrl = `http://api.airvisual.com/v2/city?city=Berlin&state=Brandenburg&country=Germany&key=${apiKey}`;
 
       // fetch air quality data
